6507881bf07cefadbc1d32387b7782dd6d5ae511
safimuhammad <safiwahid38@gmail.com>
2026-05-04 01:41:31 -0700

    Starting D sprints, D1 Agent class skeleton built Minimum scaffolding initialized

---
+++ agents/runtime.py
@@ -0,0 +1,47 @@
+from world.world import WorldState
+from bus.event_bus import EventBus
+from world.agents import AgentState, AgentStatus
+from tools.registry import ToolRegistry
+
+
+class Agent:
+    def __init__(
+        self,
+        agent_id: str,
+        world: WorldState,
+        event_bus: EventBus,
+        tool_registry: ToolRegistry,
+        model: str,
+        pace: float,
+    ) -> None:
+        self.lifecycle_history: list[dict] = (
+            []
+        )  # renamed from chat_history more suited for living breathing agents
+        self.world = world
+        self.event_bus = event_bus
+        self.tool_registry = tool_registry
+        self.model = model
+        self.pace = pace
+        self.agent_id = agent_id
+
+        self.breath_count: int = 0
+        self.alive: bool = False
+        self.agent_state: AgentState = self.world.get_agent(self.agent_id)
+        if self.agent_state:
+            self.alive = self.agent_state.status != AgentStatus.DEAD
+            self.event_bus.subscribe(self.agent_id)
+
+    async def _load_system_prompt(self):
+        pass
+
+    async def perceive(self):
+        pass
+
+    async def decide(self):
+        pass
+
+    async def execute(self, tool_calls):
+        pass
+
+    async def breathe(self):
+        pass
